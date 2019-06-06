f1fc1ac213cd21274c1a95e565df3aa007502b6e
Joshua Z. Zhang <cheungchih@gmail.com>
2019-06-06 15:19:38 -0700

    init COCO keypoints dataset with bbox

--- gluoncv/data/mscoco/keypoints.py
+++ gluoncv/data/mscoco/keypoints.py
@@ -215,3 +215,108 @@ class COCOKeyPoints(VisionDataset):
         if center[0] != -1:
             scale = scale * 1.25
         return center, scale
+
+
+class COCOBoxedkeypoints(COCOKeyPoints):
+    """COCO keypoint detection dataset with bounding boxes.
+
+    Parameters
+    ----------
+    root : str, default '~/mxnet/datasets/coco'
+        Path to folder storing the dataset.
+    splits : list of str, default ['person_keypoints_val2017']
+        Json annotations name.
+        Candidates can be: person_keypoints_val2017, person_keypoints_train2017.
+
+    """
+    CLASSES = ['person']
+
+    def __init__(self, root=os.path.join('~', '.mxnet', 'datasets', 'coco'),
+                 splits=('person_keypoints_val2017',), aspect_ratio=1.0, skip_empty=True):
+        super(COCOBoxedkeypoints, self).__init__(
+            root=root, splits=splits, aspect_ratio=aspect_ratio, skip_empty=skip_empty)
+
+    def _load_jsons(self):
+        """Load all image paths and labels from JSON annotation files into buffer."""
+        items = []
+        labels = []
+        # lazy import pycocotools
+        try_import_pycocotools()
+        from pycocotools.coco import COCO
+        for split in self._splits:
+            anno = os.path.join(self._root, 'annotations', split) + '.json'
+            _coco = COCO(anno)
+            self._coco.append(_coco)
+            classes = [c['name'] for c in _coco.loadCats(_coco.getCatIds())]
+            if not classes == self.classes:
+                raise ValueError("Incompatible category names with COCO: ")
+            assert classes == self.classes
+            json_id_to_contiguous = {
+                v: k for k, v in enumerate(_coco.getCatIds())}
+            if self.json_id_to_contiguous is None:
+                self.json_id_to_contiguous = json_id_to_contiguous
+                self.contiguous_id_to_json = {
+                    v: k for k, v in self.json_id_to_contiguous.items()}
+            else:
+                assert self.json_id_to_contiguous == json_id_to_contiguous
+            # iterate through the annotations
+            image_ids = sorted(_coco.getImgIds())
+            for entry in _coco.loadImgs(image_ids):
+                dirname, filename = entry['coco_url'].split('/')[-2:]
+                abs_path = os.path.join(self._root, dirname, filename)
+                if not os.path.exists(abs_path):
+                    raise IOError('Image: {} not exists.'.format(abs_path))
+                label = self._check_load_keypoints(_coco, entry)
+                if not label:
+                    continue
+
+                items.append(abs_path)
+                labels.append(label)
+        return items, labels
+
+    def _check_load_keypoints(self, coco, entry):
+        """Check and load ground-truth keypoints"""
+        ann_ids = coco.getAnnIds(imgIds=entry['id'], iscrowd=False)
+        objs = coco.loadAnns(ann_ids)
+        # check valid bboxes
+        valid_objs = []
+        width = entry['width']
+        height = entry['height']
+
+        for obj in objs:
+            contiguous_cid = self.json_id_to_contiguous[obj['category_id']]
+            if contiguous_cid >= self.num_class:
+                # not class of interest
+                continue
+            if max(obj['keypoints']) == 0:
+                continue
+            # convert from (x, y, w, h) to (xmin, ymin, xmax, ymax) and clip bound
+            xmin, ymin, xmax, ymax = bbox_clip_xyxy(bbox_xywh_to_xyxy(obj['bbox']), width, height)
+            # require non-zero box area
+            if obj['area'] <= 0 or xmax <= xmin or ymax <= ymin:
+                continue
+
+            # joints 3d: (num_joints, 3, 2); 3 is for x, y, z; 2 is for position, visibility
+            joints_3d = np.zeros((self.num_joints, 3, 2), dtype=np.float32)
+            for i in range(self.num_joints):
+                joints_3d[i, 0, 0] = obj['keypoints'][i * 3 + 0]
+                joints_3d[i, 1, 0] = obj['keypoints'][i * 3 + 1]
+                # joints_3d[i, 2, 0] = 0
+                visible = min(1, obj['keypoints'][i * 3 + 2])
+                joints_3d[i, :2, 1] = visible
+                # joints_3d[i, 2, 1] = 0
+                # center, scale = self._box_to_center_scale(xmin, ymin, xmax - xmin, ymax - ymin)
+
+            valid_objs.append({
+                'bbox': np.array([xmin, ymin, xmax, ymax]),
+                'joints_3d': joints_3d
+            })
+
+        if not valid_objs:
+            if not self._skip_empty:
+                # dummy invalid labels if no valid objects are found
+                valid_objs.append({
+                    'bbox': np.array([-1., -1., -1., -1.]),
+                    'joints_3d': np.zeros((self.num_joints, 3, 2), dtype=np.float32)
+                })
+        return valid_objs
