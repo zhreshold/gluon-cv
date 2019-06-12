a68ad39bf8949ba5a83ef8b4c2763dd39b7159b1
Joshua Z. Zhang <cheungchih@gmail.com>
2019-06-12 11:57:24 -0700

    backup

---
+++ gluoncv/model_zoo/free_pose/free_pose_yolo.py
@@ -0,0 +1,116 @@
+"""Free Pose Network Definition Based on YOLO"""
+from __future__ import absolute_import
+
+from mxnet.context import cpu
+from mxnet.gluon.block import HybridBlock
+from mxnet.gluon import nn
+from mxnet import initializer
+import gluoncv as gcv
+from ..yolo.yolo3 import _upsample, YOLODetectionBlockV3
+
+
+class FreePoseOutput(gluon.HybridBlock):
+    """FreePose Output.
+    Parameters
+    ----------
+    index : int
+        Index of the yolo output layer, to avoid naming conflicts only.
+    num_class : int
+        Number of foreground objects.
+    anchors : iterable
+        The anchor setting. Reference: https://arxiv.org/pdf/1804.02767.pdf.
+    stride : int
+        Stride of feature map.
+    alloc_size : tuple of int, default is (128, 128)
+        For advanced users. Define `alloc_size` to generate large enough anchor
+        maps, which will later saved in parameters. During inference, we support arbitrary
+        input image by cropping corresponding area of the anchor map. This allow us
+        to export to symbol so we can run it in c++, Scalar, etc.
+    """
+    def __init__(self, index, num_class, num_joints, anchors, stride,
+                 alloc_size=(128, 128), **kwargs):
+        super(FreePoseOutput, self).__init__(**kwargs)
+        anchors = np.array(anchors).astype('float32')
+        assert num_class == 1, "num_class should be 1(person), given {}".format(num_class)
+        self._classes = num_class
+        assert num_joints > 0, "num_joints must > 0, given {}".format(num_joints)
+        self._num_joints = num_joints
+        self._num_pred = 1 + 4 + num_class + num_joints * 2  # 1 objness + 4 box + 1 + num_joints
+        self._num_anchors = anchors.size // 2
+        self._stride = stride
+        with self.name_scope():
+            all_pred = self._num_pred * self._num_anchors
+            self.prediction = nn.Conv2D(all_pred, kernel_size=1, padding=0, strides=1)
+            # anchors will be multiplied to predictions
+            anchors = anchors.reshape(1, 1, -1, 2)
+            self.anchors = self.params.get_constant('anchor_%d'%(index), anchors)
+            # offsets will be added to predictions
+            grid_x = np.arange(alloc_size[1])
+            grid_y = np.arange(alloc_size[0])
+            grid_x, grid_y = np.meshgrid(grid_x, grid_y)
+            # stack to (n, n, 2)
+            offsets = np.concatenate((grid_x[:, :, np.newaxis], grid_y[:, :, np.newaxis]), axis=-1)
+            # expand dims to (1, 1, n, n, 2) so it's easier for broadcasting
+            offsets = np.expand_dims(np.expand_dims(offsets, axis=0), axis=0)
+            self.offsets = self.params.get_constant('offset_%d'%(index), offsets)
+
+    def hybrid_forward(self, F, x, anchors, offsets):
+        """Hybrid Forward of FreePoseOutput layer.
+        Parameters
+        ----------
+        F : mxnet.nd or mxnet.sym
+            `F` is mxnet.sym if hybridized or mxnet.nd if not.
+        x : mxnet.nd.NDArray
+            Input feature map.
+        anchors : mxnet.nd.NDArray
+            Anchors loaded from self, no need to supply.
+        offsets : mxnet.nd.NDArray
+            Offsets loaded from self, no need to supply.
+        Returns
+        -------
+        (tuple of) mxnet.nd.NDArray
+            During training, return (bbox, raw_box_centers, raw_box_scales, objness,
+            class_pred, anchors, offsets).
+            During inference, return detections.
+        """
+        # prediction flat to (batch, pred per pixel, height * width)
+        pred = self.prediction(x).reshape((0, self._num_anchors * self._num_pred, -1))
+        # transpose to (batch, height * width, num_anchor, num_pred)
+        pred = pred.transpose(axes=(0, 2, 1)).reshape((0, -1, self._num_anchors, self._num_pred))
+        # components
+        raw_box_centers = pred.slice_axis(axis=-1, begin=0, end=2)
+        raw_box_scales = pred.slice_axis(axis=-1, begin=2, end=4)
+        objness = pred.slice_axis(axis=-1, begin=4, end=5)
+        class_pred = pred.slice_axis(axis=-1, begin=5, end=5+self._num_class)
+        joints_pred = pred.slice_axis(axis=-1, begin=5+self._num_class, end=None)
+
+        # valid offsets, (1, 1, height, width, 2)
+        offsets = F.slice_like(offsets, x * 0, axes=(2, 3))
+        # reshape to (1, height*width, 1, 2)
+        offsets = offsets.reshape((1, -1, 1, 2))
+
+        box_centers = F.broadcast_add(F.sigmoid(raw_box_centers), offsets) * self._stride
+        box_scales = F.broadcast_mul(F.exp(raw_box_scales), anchors)
+        confidence = F.sigmoid(objness)
+        class_score = F.broadcast_mul(F.sigmoid(class_pred), confidence)
+        wh = box_scales / 2.0
+        bbox = F.concat(box_centers - wh, box_centers + wh, dim=-1)
+
+        if autograd.is_training():
+            # during training, we don't need to convert whole bunch of info to detection results
+            return (bbox.reshape((0, -1, 4)), raw_box_centers, raw_box_scales,
+                    objness, class_pred, anchors, offsets)
+
+        # prediction per class
+        bboxes = F.tile(bbox, reps=(self._classes, 1, 1, 1, 1))
+        scores = F.transpose(class_score, axes=(3, 0, 1, 2)).expand_dims(axis=-1)
+        ids = F.broadcast_add(scores * 0, F.arange(0, self._classes).reshape((0, 1, 1, 1, 1)))
+        detections = F.concat(ids, scores, bboxes, dim=-1)
+        # reshape to (B, xx, 6)
+        detections = F.reshape(detections.transpose(axes=(1, 0, 2, 3, 4)), (0, -1, 6))
+        return detections
+
+
+class FreePoseYOLOv3(nn.HybridBlock):
+    def __init__(self):
+        pass
