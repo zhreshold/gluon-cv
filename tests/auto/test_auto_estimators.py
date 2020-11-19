86844d8e5f70b1b94e4ca9120ab3acc502f1dc0a
Joshua Z. Zhang <cheungchih@gmail.com>
2020-11-18 21:47:48 -0800

    restrict num_workers

--- tests/auto/test_auto_estimators.py
+++ tests/auto/test_auto_estimators.py
@@ -24,10 +24,11 @@ from autogluon.core.scheduler.resource import get_cpu_count, get_gpu_count
 IMAGE_CLASS_DATASET, _, IMAGE_CLASS_TEST = ImageClassification.Dataset.from_folders(
     'https://autogluon.s3.amazonaws.com/datasets/shopee-iet.zip')
 OBJECT_DETCTION_DATASET = ObjectDetection.Dataset.from_voc('https://autogluon.s3.amazonaws.com/datasets/tiny_motorbike.zip')
+OBJECT_DETECTION_TRAIN, OBJECT_DETECTION_VAL, OBJECT_DETECTION_TEST = OBJECT_DETCTION_DATASET(train_size=0.5, val_size=0.3, test_size=0.2)
 
 def test_image_classification_estimator():
     from gluoncv.auto.estimators import ImageClassificationEstimator
-    est = ImageClassificationEstimator({'train': {'epochs': 1, 'batch_size': 8}, 'gpus': list(range(get_gpu_count()))})
+    est = ImageClassificationEstimator({'train': {'epochs': 1, 'batch_size': 8, 'num_workers': 0}, 'gpus': list(range(get_gpu_count()))})
     res = est.fit(IMAGE_CLASS_DATASET)
     assert res.get('valid_acc', 0) > 0
     test_result = est.predict(IMAGE_CLASS_TEST)
@@ -36,39 +37,35 @@ def test_image_classification_estimator():
 
 def test_center_net_estimator():
     from gluoncv.auto.estimators import CenterNetEstimator
-    est = CenterNetEstimator({'train': {'epochs': 1, 'batch_size': 8}, 'gpus': list(range(get_gpu_count()))})
-    res = est.fit(OBJECT_DETCTION_DATASET)
+    est = CenterNetEstimator({'train': {'epochs': 1, 'batch_size': 8, 'num_workers': 0}, 'gpus': list(range(get_gpu_count()))})
+    res = est.fit(OBJECT_DETECTION_TRAIN)
     assert res.get('valid_map', 0) > 0
-    _, _, test_data = OBJECT_DETCTION_DATASET.random_split()
-    test_result = est.predict(test_data)
-    evaluate_result = est.evaluate(test_data)
+    test_result = est.predict(OBJECT_DETECTION_TEST)
+    evaluate_result = est.evaluate(OBJECT_DETECTION_VAL)
 
 def test_ssd_estimator():
     from gluoncv.auto.estimators import SSDEstimator
-    est = SSDEstimator({'train': {'epochs': 1, 'batch_size': 8}, 'gpus': list(range(get_gpu_count()))})
-    res = est.fit(OBJECT_DETCTION_DATASET)
+    est = SSDEstimator({'train': {'epochs': 1, 'batch_size': 8}, , 'num_workers': 0, 'gpus': list(range(get_gpu_count()))})
+    res = est.fit(OBJECT_DETECTION_TRAIN)
     assert res.get('valid_map', 0) > 0
-    _, _, test_data = OBJECT_DETCTION_DATASET.random_split()
-    test_result = est.predict(test_data)
-    evaluate_result = est.evaluate(test_data)
+    test_result = est.predict(OBJECT_DETECTION_TEST)
+    evaluate_result = est.evaluate(OBJECT_DETECTION_VAL)
 
 def test_yolo3_estimator():
     from gluoncv.auto.estimators import YOLOv3Estimator
-    est = YOLOv3Estimator({'train': {'epochs': 1, 'batch_size': 8}, 'gpus': list(range(get_gpu_count()))})
-    res = est.fit(OBJECT_DETCTION_DATASET)
+    est = YOLOv3Estimator({'train': {'epochs': 1, 'batch_size': 8}, , 'num_workers': 0, 'gpus': list(range(get_gpu_count()))})
+    res = est.fit(OBJECT_DETECTION_TRAIN)
     assert res.get('valid_map', 0) > 0
-    _, _, test_data = OBJECT_DETCTION_DATASET.random_split()
-    test_result = est.predict(test_data)
-    evaluate_result = est.evaluate(test_data)
+    test_result = est.predict(OBJECT_DETECTION_TEST)
+    evaluate_result = est.evaluate(OBJECT_DETECTION_VAL)
 
 def test_frcnn_estimator():
     from gluoncv.auto.estimators import FasterRCNNEstimator
-    est = FasterRCNNEstimator({'train': {'epochs': 1}, 'gpus': list(range(get_gpu_count()))})
-    res = est.fit(OBJECT_DETCTION_DATASET)
+    est = FasterRCNNEstimator({'train': {'epochs': 1}, , 'num_workers': 0, 'gpus': list(range(get_gpu_count()))})
+    res = est.fit(OBJECT_DETECTION_TRAIN)
     assert res.get('valid_map', 0) > 0
-    _, _, test_data = OBJECT_DETCTION_DATASET.random_split()
-    test_result = est.predict(test_data)
-    evaluate_result = est.evaluate(test_data)
+    test_result = est.predict(OBJECT_DETECTION_TEST)
+    evaluate_result = est.evaluate(OBJECT_DETECTION_VAL)
 
 if __name__ == '__main__':
     import nose
